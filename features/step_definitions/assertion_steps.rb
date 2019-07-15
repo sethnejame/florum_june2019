d8c6b27fa8d510c5e0db3e8e7d1c23e3f248cb19
Seth NeJame <sethnejame@gmail.com>
2019-07-15 16:17:06 +0200

    Fixing steps

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -3,6 +3,6 @@ Then("I should see {string}") do |content|
     expect(page).to have_content content
   end
 
-Then("I should be on {string} page") do |content|
+Then("I should be on the {string} page") do |content|
     expect(page).to have_content content
 end
