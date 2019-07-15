87732e5fce536637ba3a6163a1789ac2492148c5
Seth NeJame <sethnejame@gmail.com>
2019-07-15 15:59:08 +0200

    Adding steps files

---
+++ features/step_definitions/assertion_steps.rb
@@ -0,0 +1,8 @@
+  
+Then("I should see {string}") do |content|
+    expect(page).to have_content content
+  end
+
+Then("I should be on {string} page") do |content|
+    expect(page).to have_content content
+end
