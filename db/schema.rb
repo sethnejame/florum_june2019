b059309ca69757e4c270278b66664a70636c96ab
Seth NeJame <sethnejame@gmail.com>
2019-07-16 20:06:42 +0200

    Wired up Create Profile in navbar to new_profile_path, added user_id to profile model in DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_174826) do
+ActiveRecord::Schema.define(version: 2019_07_16_180501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_07_16_174826) do
     t.string "profile_pic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
