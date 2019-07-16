80f5f5ca8e8328f31ae1cd1863299bb22ac75edf
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-16 16:43:22 +0200

    fixes resources for routes and fixes small change in new.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root controller: :main, action: :index
-  resources :posts, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :posts
 end
