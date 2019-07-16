6f75f04924bad1627a76f0903b18904a89bdb2fe
Julie Ahlquist <julieahlquist@hotmail.com>
2019-07-16 21:56:53 +0200

    deletes exessive assertion step

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -9,8 +9,3 @@ end
 Then("I should be on Posts index page") do
   expect(current_path).to eq posts_path
 end
-
-Then("I should be on {string} page") do |page|
-  post = Post.find_by(title: page)
-  expect(current_path).to eq posts_path(posts_path)
-end
