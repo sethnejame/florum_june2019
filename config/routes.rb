225c88c3a9dafd60a04ac51a184dad8953276289
pedro_bras <pedro.c.bras@gmail.com>
2019-07-18 13:39:03 +0200

    adds delete path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   root controller: :main, action: :index
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index, :destroy]
 end
