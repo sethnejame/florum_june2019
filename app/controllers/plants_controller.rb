15e291615cd84c427d66dcd76cc786162cd4c9a7
Seth NeJame <sethnejame@gmail.com>
2019-07-18 14:52:07 +0200

    User plant show up on plant index now

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,26 +1,26 @@
 class PlantsController < ApplicationController
 
-    def index
-        @plants = Plant.all
-      end
+  def index
+    @plant = current_user.plants.all
+  end
     
-      def new
-        @plant = Plant.new
-      end
+  def new
+    @plant = Plant.new
+  end
     
-      def create
-        @plant = current_user.plants.create(plant_params)
+  def create
+    @plant = current_user.plants.create(plant_params)
     
-        if @plant.persisted?
-          flash[:notice] = 'Your plant is now for sale!'
-          redirect_to user_path(current_user)
-        else
-          flash[:alert] = 'Invalid Title or Text.'
-          redirect_to new_plant_path
-        end
-      end
+    if @plant.persisted?
+      flash[:notice] = 'Your plant is now for sale!'
+      redirect_to user_path(current_user)
+    else
+      flash[:alert] = 'Invalid entry.'
+      redirect_to new_plant_path
+    end
+  end
 
-      def plant_params
-        params.require(:plant).permit(:name, :details, :price, :category, :quantity)
-      end
+  def plant_params
+    params.require(:plant).permit(:name, :details, :price, :category, :quantity)
+  end
 end
