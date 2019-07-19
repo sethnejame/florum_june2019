c19d1ce59ccf8b6ed951ba898155294b64cc4ce6
Seth NeJame <sethnejame@gmail.com>
2019-07-19 11:44:57 +0200

    Adding category to post params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,6 +44,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :text)
+    params.require(:post).permit(:title, :text, :category)
   end
 end
